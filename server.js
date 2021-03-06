6575f7e48491b137ed3b7f404074a8dec5c87fe5
Adam L <alane019@gmail.com>
2021-03-05 21:57:28 -0500

    undo some changes to server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
 
-const PORT = process.env.PORT || 80;
+const PORT = process.env.PORT || 3000;
 
 const app = express();
 
@@ -17,14 +17,18 @@ app.use(express.static("public"));
 
 mongoose.connect(
   process.env.MONGODB_URI || 'mongodb://localhost/budget',
-{
-  useNewUrlParser: true,
-  useFindAndModify: false
-});
+  {
+	  useNewUrlParser: true,
+	  useUnifiedTopology: true,
+	  useCreateIndex: true,
+	  useFindAndModify: false
+  }
+);
+
 
 // routes
 app.use(require("./routes/api.js"));
 
 app.listen(PORT, () => {
-  console.log(`App running on port ${PORT}! (http://localhost:${PORT})`);
+  console.log(`App running on port ${PORT}!`);
 });
